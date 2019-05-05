913c621f1ba7d642472b00c312b6b206d451421e
huehou <Alexander_hue1993@hotmail.com>
2019-05-05 16:02:40 +0800

    Figure2.4 is born

--- Chapter 2/Figure 2.4.py
+++ Chapter 2/Figure 2.4.py
@@ -8,6 +8,7 @@ b2 = 4 * 10**(-5)
 g = 9.81
 angleDeg = [30, 35, 40, 45, 50, 55]
 angleRad = [i*np.pi/180 for i in angleDeg]
+theta = [(30,'-'), (35,':'), (40,'-.'), (45,'--'), (50,'--'), (55,'-')];
 
 # ======================
 # Without Drag
@@ -27,7 +28,7 @@ for i, angle in enumerate(angleRad):
         vy = vy - g*dt
         v = np.sqrt(vx**2 + vy**2)
     
-    plt.plot(np.array(x)/1000, np.array(y)/1000)
+    plt.plot(np.array(x)/1000, np.array(y)/1000, theta[i][1])
 
 plt.xlabel('$x$ (km)')
 plt.ylabel('$y$ (km)')
@@ -35,12 +36,17 @@ plt.title('Trajectory of cannon shell')
 plt.tick_params(direction = 'in', right = True, top = True)
 plt.xlim(0,60)
 plt.ylim(0,20)
+plt.text(45, 17.5, 'No drag', size = 14)
+
+for i, (angle, style) in enumerate(theta[:-1]):
+    plt.text(20, 5.2 + i*2, str(angle) + '$^o$', size = 14)
+plt.text(20, 17.5, '55$^o$', size = 14)
 plt.figure()
 # ================
 # With drag
 # ================
 
-for angle in angleRad:
+for i, angle in enumerate(angleRad):
     v = 700
     vx = v*np.cos(angle)
     vy = v*np.sin(angle)
@@ -55,7 +61,15 @@ for angle in angleRad:
         vy = vy - g*dt - b2*v*vy*dt
         v = np.sqrt(vx**2 + vy**2)
     
-    plt.plot(x, y)
+    plt.plot(np.array(x)/1000, np.array(y)/1000, theta[i][1])
+
+plt.xlim(0,60)
+plt.ylim(0,20)
+plt.text(40, 17.5, 'With air drag', size = 14)
+plt.title('Trajectory of canon shell')
+plt.tick_params(direction = 'in', top = True, right = True)
+plt.xlabel("$x$ (km)")
+plt.ylabel("$y$ (km)")
 
 plt.show()
 
