8fc08616401d841bd0297d0d912f211e86a6b599
rajesh-mishra95 <rajesh.nts2009@gmail.com>
2020-11-30 23:10:25 +0800

    Added the runtime comparison

--- Chapter 1/Exercise1.1.jl
+++ Chapter 1/Exercise1.1.jl
@@ -1,5 +1,17 @@
 using PyPlot
 
+function euler(f0::Float64, df::Float64, dt::Float64, tend::Float64)
+    n::Int64 = Int(tend/dt) + 1
+    f::Vector{Float64} = zeros(n)
+    t::Vector{Float64} = zeros(n)
+    for i = 1:n-1
+        f[i+1] = f[i] + df*dt
+        t[i+1] = t[i] + dt
+    end
+    return t,f
+end
+
+#=
 function euler(f0, df, dt, tend)
     f = [f0]
     t = [0]
@@ -10,9 +22,10 @@ function euler(f0, df, dt, tend)
     end
     return t,f
 end
+=#
 
 function main()
-    t,v = euler(0.0, -9.8, 1.0, 10.0)
+    t,v = euler(0.0, -9.8, 0.1, 10.0)
     clf()
     plot(t,v)
     xlabel("t")
@@ -20,3 +33,10 @@ function main()
     title("Chapter 1 Exercise 1.1")
     display(gcf())
 end
+
+# @time euler(0.0, -9.8, 1.0, 10.0)
+
+# using the simple implementation of euler took ~70% more time
+# than the slightly complicated implementation. The time and 
+# memory allocation can be checked by commenting out the line 
+# containing @time macro.
