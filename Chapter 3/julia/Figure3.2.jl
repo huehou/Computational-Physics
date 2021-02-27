cdeca86354df34553397b760c65347626bef43c0
huehou <Alexander_hue1993@hotmail.com>
2021-02-27 13:52:35 +0800

    Modified julia code to function approach

--- Chapter 3/julia/Figure3.2.jl
+++ Chapter 3/julia/Figure3.2.jl
@@ -1,7 +1,5 @@
-# using Plots
 using LaTeXStrings
 using PyPlot
-# pyplot()
 
 g = 9.8
 l = 1.
@@ -9,41 +7,48 @@ dt = 0.04
 theta0 = 0.2
 omega0 = 0.
 
-theta = [theta0]
-t = [0.]
-omega = omega0
-
-while t[end] <= 10
-	omega1 = omega - g/l*theta[end]*dt
-	push!(theta, theta[end] + omega*dt)
-	push!(t, t[end]+dt)
-	global omega = omega1
+function euler_1step(theta0::Float64, omega0::Float64, dt::Float64)
+	theta1::Float64 = theta0 + omega0*dt
+	omega1::Float64 = omega0 - g/l*theta0*dt
+	theta1, omega1
 end
 
-# res = plot(t, theta, 
-		   # linewidth = 2,
-		   # xlim = (0, 10),
-		   # ylim = (-2,2),
-		   # framestyle = :box,
-		   # grid = :none,
-		   # xlabel = "time (s)",
-		   # ylabel = "\$\\theta\$ (radians)",
-		   # legend = false)
+function euler(theta0::Float64, omega0::Float64, dt::Float64, tend::Float64)
+	n::Int64 = Int(tend/dt) + 1
+	theta::Vector{Float64} = zeros(n)
+	t::Vector{Float64} = zeros(n)
+	theta[1] = theta0
+	omega1::Float64 = omega0
+	for i = 1:n-1
+		theta[i+1], omega1 = euler_1step(theta[i], omega1, dt)
+		t[i+1] = t[i] + dt
+	end
+	t, theta
+end
 
-res = plot(t, theta)
-xlabel("time (s)")
-ylabel("\$\\theta\$ (radians)")
-xlim([0,10])
-ylim([-2,2])
+function main()
+	t, theta = euler(theta0, omega0, dt, 10.)
 
-ax = gca()
-ax[:tick_params](direction = "in", right = true, top = true)
-xticks([0,2,4,6,8,10])
-yticks([-2,-1,0,1,2])
-annotate("Simple Pendulum - Euler method", xy = (1, 1.5), fontsize = 14)
-annotate("Length = 1m    time step = 0.04 s", xy = (1, 1.2), fontsize = 14)
+	res = plot(t, theta)
+	xlabel("time (s)")
+	ylabel("\$\\theta\$ (radians)")
+	xlim([0,10])
+	ylim([-2,2])
 
-savefig("Figure3.2.pdf", bbox_inches = "tight")
+	ax = gca()
+	ax[:tick_params](direction = "in", right = true, top = true)
+	xticks([0,2,4,6,8,10])
+	yticks([-2,-1,0,1,2])
+	annotate("Simple Pendulum - Euler method", xy = (1, 1.5), fontsize = 14)
+	annotate("Length = 1m    time step = 0.04 s", xy = (1, 1.2), fontsize = 14)
+
+	savefig("Figure3.3.pdf", bbox_inches = "tight")
+	
+end
+
+main()
+
+
 
 # pygui(true)
 # show()
