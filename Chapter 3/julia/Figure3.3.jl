4f79fd2ca285bf42495727d88a9478468b4cf92a
Alexander Hue <alexander_hue1993@hotmail.com>
2021-02-27 14:02:58 +0800

    Figure 3.3 is born

---
+++ Chapter 3/julia/Figure3.3.jl
@@ -0,0 +1,52 @@
+using LaTeXStrings
+using PyPlot
+
+g = 9.8
+l = 1.
+dt = 0.04
+theta0 = 0.2
+omega0 = 0.
+
+function euler_1step(theta0::Float64, omega0::Float64, dt::Float64)
+	omega1::Float64 = omega0 - g/l*theta0*dt
+	theta1::Float64 = theta0 + omega1*dt
+	theta1, omega1
+end
+
+function euler(theta0::Float64, omega0::Float64, dt::Float64, tend::Float64)
+	n::Int64 = Int(tend/dt) + 1
+	theta::Vector{Float64} = zeros(n)
+	t::Vector{Float64} = zeros(n)
+	theta[1] = theta0
+	omega1::Float64 = omega0
+	for i = 1:n-1
+		theta[i+1], omega1 = euler_1step(theta[i], omega1, dt)
+		t[i+1] = t[i] + dt
+	end
+	t, theta
+end
+
+function main()
+	t, theta = euler(theta0, omega0, dt, 10.)
+
+	res = plot(t, theta)
+	xlabel("time (s)")
+	ylabel("\$\\theta\$ (radians)")
+	xlim([0,10])
+	ylim([-0.3,0.3])
+
+	ax = gca()
+	ax[:tick_params](direction = "in", right = true, top = true)
+	xticks([0,2,4,6,8,10])
+	yticks([-0.3,-0.2,-0.1,0,0.1,0.2,0.3])
+	title("Simple Pendulum - Euler-Cromer method", fontsize = 14)
+	annotate("Length = 1m    time step = 0.04 s", xy = (1.7, 0.25), fontsize = 14)
+	# annotate("Simple Pendulum - Euler method", xy = (1, 1.5), fontsize = 14)
+	# annotate("Length = 1m    time step = 0.04 s", xy = (1, 1.2), fontsize = 14)
+
+	savefig("Figure3.3.pdf", bbox_inches = "tight")
+	
+end
+
+main()
+
